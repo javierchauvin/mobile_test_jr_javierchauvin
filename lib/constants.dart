db101a39ec55d168df1cd728a030a95bc81ab61f
JavierChauvinDevsu <javier.chauvin@devsu.com>
2024-05-08 02:32:02 -0500

    Presentación de timestamp de manera minimalista

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,56 @@
 import 'package:flutter/material.dart';
 
+enum MonthsMMM{
+  NO_MONTH,
+  JANUARY,
+  FEBRUARY,
+  MARCH,
+  APRIL,
+  MAY,
+  JUNE,
+  JULY,
+  AUGUST,
+  SEPTEMBER,
+  OCTOBER,
+  NOVEMBER,
+  DECEMBER,
+}
+
+String int2MMM(MonthsMMM month){
+  String mmm = "";
+
+  switch(month){
+    case MonthsMMM.JANUARY:
+      mmm = "ENE";
+    case MonthsMMM.FEBRUARY:
+      mmm = "FEB";
+    case MonthsMMM.MARCH:
+      mmm = "MAR";
+    case MonthsMMM.APRIL:
+      mmm = "ABR";
+    case MonthsMMM.MAY:
+      mmm = "MAY";
+    case MonthsMMM.JUNE:
+      mmm = "JUN";
+    case MonthsMMM.JULY:
+      mmm = "JUL";
+    case MonthsMMM.AUGUST:
+      mmm = "AGO";
+    case MonthsMMM.SEPTEMBER:
+      mmm = "SEP";
+    case MonthsMMM.OCTOBER:
+      mmm = "OCT";
+    case MonthsMMM.NOVEMBER:
+      mmm = "NOV";
+    case MonthsMMM.DECEMBER:
+      mmm = "DIC";
+
+    case MonthsMMM.NO_MONTH:
+      mmm = "ERROR";
+  }
+  return mmm;
+}
+
 class LocalColors extends Color {
   LocalColors() : super(100);
 
@@ -7,3 +58,5 @@ class LocalColors extends Color {
   static const shadow = const Color(0xFF3D3D3D);
   static const background = const Color(0xFFFCFCFC);
 }
+
+
--- lib/pages/iss_position_tracker_page.dart
+++ lib/pages/iss_position_tracker_page.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:intl/intl.dart';
 
 import '../api/issPosition_API.dart';
 import '../models/iss_location_model.dart';
@@ -30,24 +31,15 @@ class _IssPositionPageState extends State<IssPositionPage> {
         });
       }
     });
+
   }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
 
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: LocalColors.shadow,
-        title: const Text(
-            'Posición Actual ISS',
-              style: TextStyle(color: LocalColors.background),
-        ),
-      ),
-      body: Center(child:
+  Widget _map(){
+    return Center(child:
       FutureBuilder<IssLocation>(
         future: newIssLocation,
         builder: (context, snapshot) {
@@ -74,12 +66,50 @@ class _IssPositionPageState extends State<IssPositionPage> {
           } else if (snapshot.hasError) {
             return Text('${snapshot.error}');
           }
-
-          // By default, show a loading spinner.
           return const CircularProgressIndicator();
         },
       )
+    );
+  }
+
+  Widget _timeStamp(){
+    return Container(
+      alignment: Alignment.bottomCenter,
+      padding: const EdgeInsets.all(5.0),
+      child: FutureBuilder<IssLocation>(
+          future:newIssLocation,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(
+                DateFormat('MMM d, k:mm a').format(snapshot.data!.timestamp)
+              );
+            }else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+            return const LinearProgressIndicator();
+          }
       )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: LocalColors.shadow,
+        title: const Text(
+            'Posición Actual ISS',
+              style: TextStyle(color: LocalColors.background),
+        ),
+      ),
+      body:
+      Stack(
+        children: <Widget>[
+          _map(),
+          _timeStamp(),
+        ],
+      ),
     );
 
   }
